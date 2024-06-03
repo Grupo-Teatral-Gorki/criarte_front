474fe68560bdc6c3df12673d6f4c4339fd085078
Pedro Henrique (Styxx) <96383874+pedrostyxx@users.noreply.github.com>
2024-06-02 21:10:28 -0300

    Update

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">Logo Criarte</div>
+      <div className="hamburger-menu">&#9776;</div>
+    </header>
+  );
+}
+
+export default Header;
